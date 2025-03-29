c2bd2b039b14793720e1de82c853e9c6a4cc9419
Ayush <sinhaayush1125@gmail.com>
2025-03-30 01:52:39 +0530

    another initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
+import Body from "./components/Body";
 
 export default function App() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-gray-900 text-white text-3xl">
-      Hello, Vite + React!
-    </div>
-  );
+  return <Body />;
 }
---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './Login'
+
+const Body = () => {
+    return (
+        <div>
+            <Login />
+            <Browse />
+        </div>
+    )
+}
+
+export default Body
