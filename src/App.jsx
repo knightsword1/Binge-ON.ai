27cb6bade705a91ad7cb73c41f8bf20b7304016a
Ayush <sinhaayush1125@gmail.com>
2025-03-30 01:37:19 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 text-white text-3xl">
+      Hello, Vite + React!
+    </div>
+  );
+}
